9dad2f002ba04eea6d69d28c78a7b6ed2bebd2c7
Martin Kim Dung-Pham <kim@elbedev.com>
2017-11-22 10:19:39 +0100

    Add method to CrashSource abstract class

--- lib/crep/model/crash_sources/crash_source.rb
+++ lib/crep/model/crash_sources/crash_source.rb
@@ -2,10 +2,17 @@ require 'hockeyapp'
 
 module Crep
 
+  # This is an abstract class that defines the methods which subclasses must implement.
   class CrashSource
 
+    # Necessary configuration of the subclass can happen here
     def configure
       raise 'CrashSource subclass has to implement the `configure` method.'
+    end
+
+    # The app method returns the app of the CrashSource
+    def app
+      raise 'CrashSource subclass has to implement the `app` method.'
     end
 
   end
