f481503f5cccd6ea3c5680ed271bc7c871339e54
Serghei Moret <serghei.moret@xing.com>
2017-12-11 15:28:57 +0100

    test_

--- spec/lib/crep/crash_controller.rb
+++ spec/lib/crep/crash_controller.rb
@@ -94,4 +94,49 @@ Reason: Unknown\n"
       expect(result).to eql([['Class: SomeWeirdClass', 'Occurrences: 50', 'Percentage: 12.5% of all 0.1.14 crashes', 'File/Line: line:123', 'Reason: Unknown'], ['Class: SomeOtherClass', 'Occurrences: 100', 'Percentage: 25.0% of all 0.1.14 crashes', 'File/Line: line:123', 'Reason: Unknown']])
     end
   end
+
+  describe 'top_crashes' do
+    let(:crash_controller) { subject.new }
+    let(:line) { 'line:123' }
+    let(:crash_occurances) { 50 }
+    let(:crash_occurances_2) { 100 }
+    let(:crash_reason) { 'Unknown' }
+    let(:crash_class_) { 'SomeWeirdClass' }
+    let(:crash_class_2) { 'SomeOtherClass' }
+    let(:crash_instance) { Crep::Crash.new(file_line: line, occurrences: crash_occurances, reason: crash_reason, crash_class: crash_class_) }
+    let(:crash_instance_2) { Crep::Crash.new(file_line: line, occurrences: crash_occurances_2, reason: crash_reason, crash_class: crash_class_2) }
+    let(:expected_output) do
+      "Reporting for app_name (0.1.19/1114) bundle.id\n
+------------- #1 --------------
+Class: SomeWeirdClass
+Occurrences: 50
+Percentage: 33.33% of all 0.1.19 crashes
+File/Line: line:123
+Reason: Unknown\n
+------------- #2 --------------
+Class: SomeOtherClass
+Occurrences: 100
+Percentage: 66.67% of all 0.1.19 crashes
+File/Line: line:123
+Reason: Unknown\n"
+    end
+
+    before do
+      allow(@crash_source).to receive(:crashes).and_return([crash_instance, crash_instance_2])
+      allow(@crash_source).to receive(:crash_count).and_return(150)
+      allow(@crash_source).to receive(:app)
+      allow(@crash_source.app).to receive(:name).and_return('app_name')
+      allow(@crash_source.app).to receive(:bundle_identifier).and_return('bundle.id')
+    end
+
+    it 'should output the result based on class variables' do
+      expect do
+        subject.new.top_crashes('0.1.19', 1114)
+      end.to output(expected_output).to_stdout
+    end
+
+    it 'should receive info from @crash_source and return result' do
+      expect(subject.new.top_crashes('0.1.19', 1114)).to eql([['Class: SomeWeirdClass', 'Occurrences: 50', 'Percentage: 33.33% of all 0.1.19 crashes', 'File/Line: line:123', 'Reason: Unknown'], ['Class: SomeOtherClass', 'Occurrences: 100', 'Percentage: 66.67% of all 0.1.19 crashes', 'File/Line: line:123', 'Reason: Unknown']])
+    end
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require 'crep'
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
-
+  RSpec::Mocks.configuration.allow_message_expectations_on_nil = true
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
