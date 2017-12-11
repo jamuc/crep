46a8c271c99af764bf8de55c0ee29ad7bf40deb3
Serghei Moret <serghei.moret@xing.com>
2017-12-11 10:17:49 +0100

    Add first unit tests

---
+++ spec/lib/crep/crash_controller.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+RSpec.describe Crep::CrashController do
+  subject do
+    Class.new(Crep::CrashController) do
+      def initialize; end
+    end
+  end
+
+  describe 'crash_report' do
+    let(:crash_controller) { subject.new }
+    let(:line) { 'line:123' }
+    let(:crash_occurances) { 125 }
+    let(:crash_reason) { 'Unknown' }
+    let(:crash_class_) { 'SomeWeirdClass' }
+    let(:crash_instance) { Crep::Crash.new(file_line: line, occurrences: crash_occurances, reason: crash_reason, crash_class: crash_class_) }
+    let(:random_array) { %w[some random array] }
+    let(:crash_percentage) { 5.124 }
+    let(:app_version) { '1.0.19' }
+
+    it 'should return a correct result' do
+      result = crash_controller.crash_report(crash: crash_instance, percentage: crash_percentage, version: app_version)
+      expect(result).to eql(["Class: #{crash_class_}", "Occurrences: #{crash_occurances}", "Percentage: #{crash_percentage.round(2)}% of all #{app_version} crashes", "File/Line: #{line}", "Reason: #{crash_reason}"])
+    end
+
+    it 'should raise when crash is not defined correctly' do
+      expect do
+        crash_controller.crash_report(crash: random_array, percentage: crash_percentage, version: app_version)
+      end.to raise_exception('Crash info does not fulfill the requirements')
+    end
+  end
+
+  describe 'crash_percentage' do
+    let(:line) { 'line:123' }
+    let(:crash_occurances) { 50 }
+    let(:crash_reason) { 'Unknown' }
+    let(:crash_class_) { 'SomeWeirdClass' }
+    let(:crash_instance) { Crep::Crash.new(file_line: line, occurrences: crash_occurances, reason: crash_reason, crash_class: crash_class_) }
+    let(:total) { 150 }
+    it 'should calculate crash percentage' do
+      result = subject.new.crash_percentage(crash: crash_instance, total_crashes: total)
+      expect(result).to eql(crash_occurances / total.to_f * 100)
+    end
+  end
+
+  describe 'crash_report' do
+  end
+end
