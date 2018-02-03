be03b945d6f667e4b4aab4d4b4750da99a6571fa
Martin Kim Dung-Pham <kim@elbedev.com>
2018-02-03 16:02:11 +0100

    Add filter parameters
    Signed-off-by: Martin Kim Dung-Pham <kim@elbedev.com>

--- lib/crep/command/apps.rb
+++ lib/crep/command/apps.rb
@@ -3,26 +3,40 @@ require 'crep/app_controller'
 require 'crep/model/app_source/hockeyapp_app_source'
 
 module Crep
-    class Apps < Command
+  class Apps < Command
+    # options should accept app name and version filter
+    DEFAULT_VERSIONS_LIMIT = 5
 
-        # options should accept app name and version filter
-  
-        self.summary = <<-DESC
-            Shows the apps available for the current API token
-        DESC
-
-        self.description = <<-DESC
-            The CREP_HOCKEY_API_TOKEN needs to be set.
-        DESC
-
-        def initialize(argv)
-            super
-        end
-
-        def run
-            app_source = HockeyAppAppSource.new
-            app_controller = AppController.new(app_source)
-            app_controller.apps()
-        end
+    def self.options
+      [
+        ['--identifier=<com.company.app>', 'Crep will show app information with this identifier'],
+        ['--version=<7.10.0>', 'An optional filter for the version of an app.'],
+        ['--build=<24>', 'An optional filter for the build of an App.'],
+        ['--versions=5', "If set, Crep will limit the number of displayed versions per app. #{DEFAULT_VERSIONS_LIMIT} by default."]
+      ].concat(super)
     end
+
+    self.summary = <<-DESC
+          Shows the apps with their versions available for the current API token
+      DESC
+
+    self.description = <<-DESC
+        Shows apps with their versions. The CREP_HOCKEY_API_TOKEN needs to be set.
+      DESC
+
+    def initialize(argv)
+      super
+
+      @versions = argv.option('versions').to_i || DEFAULT_VERSIONS_LIMIT
+      @identifier = argv.option('identifier')
+      @version = argv.option('version')
+      @build = argv.option('build')
+    end
+
+    def run
+      app_source = HockeyAppAppSource.new
+      app_controller = AppController.new(app_source)
+      app_controller.apps
+    end
+  end
 end
