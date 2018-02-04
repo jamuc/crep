c4ad94ca95b53aa871827ac49b383a3175a94b76
Martin Kim Dung-Pham <kim@elbedev.com>
2018-02-04 10:54:21 +0100

    Run Rubocop

--- lib/crep/model/app_source/app_source.rb
+++ lib/crep/model/app_source/app_source.rb
@@ -6,14 +6,14 @@ module Crep
 
     def configure
       CrepLogger.info('Configuration of the CrashSource can happen in the `configure` method')
-      end
+    end
 
     def apps
       raise 'AppSource subclass has to implement the `apps` method.'
-      end
+    end
 
     def versions(_app)
       raise 'AppSource subclass has to implement the `versions` method.'
-      end
+    end
   end
 end
